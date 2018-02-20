58bdfae9c1463a40e4346d209647cd49ecd82a2c
Labean1 <1qwop1290@gmail.com>
2018-02-20 14:28:14 +0200

    My new local git

---
+++ test.js
@@ -0,0 +1,55 @@
+
+var land = document.createElement("div"),
+    field = document.createElement("div"),
+    scoreBoard = document.createElement("div"),
+    listOfWarriors = document.createElement("div");
+    
+
+    land.classList.add("land");
+    listOfWarriors.classList.add("list");
+    field.classList.add("field");
+    scoreBoard.classList.add("score");
+
+    scoreBoard.innerHTML = "scoreBoard";
+    
+    for(i=0;i<80;i++){
+       var fieldCell = document.createElement("div");
+        fieldCell.classList.add("fieldCell");
+        fieldCell.innerHTML = i;
+        field.appendChild(fieldCell);
+    }
+     
+
+newSquad1.squad.forEach(function(res) {
+    var warriorBlock=document.createElement("div"),
+        warriorTitel=document.createElement("div"),
+        warriorBody=document.createElement("ul");
+
+
+    warriorBlock.classList.add("warrior");
+
+    warriorTitel.classList.add("warrior-list");
+    warriorTitel.innerHTML = res.type;
+
+    warriorBody.classList.add("warrior-body")
+
+
+   for (var key in res){
+    if(res.hasOwnProperty(key)) {
+        var liElement = document.createElement("li");
+         liElement.innerHTML = key + ": " + res[key];
+        warriorBody.appendChild(liElement);
+    }
+    }
+
+    warriorBlock.appendChild(warriorTitel);
+    warriorBlock.appendChild(warriorBody);
+
+    listOfWarriors.appendChild(warriorBlock);
+
+ 
+});
+    land.appendChild(listOfWarriors);
+    land.appendChild(field);
+    land.appendChild(scoreBoard); 
+    container.appendChild(land);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var join = require("path").join;
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry:"./input.js",
+    output:{
+        path: join(__dirname, "./dist"),
+        filename: "[name].bundle.js"
+    },
+  
+    module:{
+        rules:[
+            {test:/\.hbs$/, use:"html-loader"},
+            {test:/\.css$/, use: ExtractTextPlugin.extract("css-loader")}
+        ]
+    },
+    plugins:[
+        new ExtractTextPlugin("styleMilitary.css"),
+        new HtmlWebpackPlugin({
+            template: "./military.html"
+        })
+    ]
+}
